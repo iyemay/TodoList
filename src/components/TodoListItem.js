59f24d84ba3f19f752333d02c8863ded2cd83011
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-03 23:42:50 -0500

    Added dummy function removeTask

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,14 +2,23 @@ import React, {Component} from "react";
 
 export class TodoListItem extends Component {
 
+    removeTask(e) {
+        e.preventDefault();
+        {
+            console.log("Remove task");
+        }
+    }
+
     render() {
         const task = this.props.task;
-        console.log(task);
         return (
             <div className="d-flex py-3 px-3 mx-5 my-5 border border-danger align-items-center
-                        justify-content-between">
+                        justify-content-between" key={task.id}>
                 <h4 className="mb-0">{task.textTodo}</h4>
-                <img alt="remove" src="../icons/remove.svg" width="25" height="25"/>
+                <form>
+                    <input type="image" alt="remove" src="icons/remove.svg" width="25" height="25"
+                           onClick={this.removeTask.bind(this)}/>
+                </form>
             </div>
         );
     }
