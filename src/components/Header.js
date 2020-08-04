bc559ec4ab8eaafaf74957ac37e80b5539272d97
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-04 12:44:38 -0500

    Added the header-text class to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React, {Component} from "react";
 export class Header extends Component {
     render() {
         return (
-            <div className="d-flex flex-column justify-content-center mt-5">
+            <div className="d-flex justify-content-center flex-column align-items-center mt-5">
                 <img alt="logo" src="icons/ico-logo.png" width="50" height="50"/>
-                <h4>Frontend Test</h4>
+                <strong><p className="mt-3 header-text">Frontend Test</p></strong>
             </div>
         );
     }
