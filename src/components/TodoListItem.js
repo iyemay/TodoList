11fe2cc40da268fe07a51d50b9e6a6d062c0ecf5
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-03 19:31:14 -0500

    Creating the TodoListItem component

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TodoListItem = () => {
+    return (
+        <div className="d-flex py-5 px-5 mx-5 my-5 border border-danger">
+            <p>Tarea 1</p>
+        </div>
+    );
+}
+
+
