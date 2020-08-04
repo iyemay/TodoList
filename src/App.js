30c0e170e5d5378cc8ce1c347776ebaa89b30546
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-03 22:43:52 -0500

    Including the TodoList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
-import { TodoListItem } from "./components/TodoListItem";
+import { TodoList } from "./components/TodoList";
 
-function App() {
-    return (
-        <TodoListItem />
-    );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: [
+                {
+                    id: 1,
+                    textTodo: "Tarea 1"
+                },
+                {
+                    id: 2,
+                    textTodo: "Tarea 2"
+                },
+                {
+                    id: 3,
+                    textTodo: "Tarea 3"
+                }
+            ]
+        };
+    }
+
+
+    render() {
+        return(
+            <TodoList todoList={this.state.todoList}/>
+        );
+    }
 }
 
 export default App;
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {Component} from "react";
 
-export const TodoListItem = () => {
-    return (
-        <div className="d-flex py-5 px-5 mx-5 my-5 border border-danger">
-            <p>Tarea 1</p>
-        </div>
-    );
+export class TodoListItem extends Component {
+
+    render() {
+        const task = this.props.task;
+        console.log(task);
+        return (
+            <div className="d-flex py-3 px-3 mx-5 my-5 border border-danger align-items-center
+                        justify-content-between">
+                <h4 className="mb-0">{task.textTodo}</h4>
+                <img alt="remove" src="../icons/remove.svg" width="25" height="25"/>
+            </div>
+        );
+    }
 }
-
-
