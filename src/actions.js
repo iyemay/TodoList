604801ac28cda28744e986883833329a0ec11e6a
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-04 19:31:23 -0500

    Created the actions and actions creators

---
+++ src/actions.js
@@ -0,0 +1,24 @@
+export const ADD_TODO = 'ADD_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const REMOVE_ALL_TODO = 'REMOVE_ALL_TODO'
+
+export function addTodoAction(todoText) {
+    return {
+        type: ADD_TODO,
+        todoText: todoText,
+        todoId: uuid()
+    }
+}
+
+export function removeTodoAction(todoId) {
+    return {
+        type: REMOVE_TODO,
+        todoId: todoId
+    }
+}
+
+export function removeAllTodo() {
+    return {
+        type: REMOVE_ALL_TODO
+    }
+}
