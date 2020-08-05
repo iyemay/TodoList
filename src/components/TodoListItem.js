5e3011d57820d8baf599f22d345d5fb9940a1654
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-05 09:50:06 -0500

    Added the removeTodo method and mapDispatchProps

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,15 +1,21 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {removeAllTodoAction, removeTodoAction} from "../actions";
 
 class TodoListItem extends Component {
+
+    removeTodo = (todoId) => {
+        console.log(todoId);
+        this.props.removeTodoAction(todoId);
+    }
 
     render() {
         const task = this.props.task;
         return (
             <div className="d-flex py-3 px-3 mx-5 align-items-center
-                        justify-content-between item-todo" key={task.id}>
+                        justify-content-between item-todo" key={task.todoId}>
                 <h4 className="mb-0">{task.todoText}</h4>
-                <form>
+                <form onSubmit={(event) => this.removeTodo(task.todoId)}>
                     <input type="image" alt="remove" src="icons/remove.svg" width="25" height="25"/>
                 </form>
             </div>
@@ -28,4 +34,8 @@ function mapStateToProps(state, ownProps) {
     };
 }
 
-export default connect(mapStateToProps)(TodoListItem);
+const mapDispatchToProps = {
+    removeTodoAction
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoListItem);
