560b9239a59e8a0bb4dc2af2b4c91522f518102b
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-05 01:23:47 -0500

    Corrected the state of method reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,23 @@
 import { ADD_TODO, REMOVE_TODO, REMOVE_ALL_TODO} from "./actions";
 
-const initialState = { todos: [] };
+const initialState = { todoList: [] };
 
 export default function todosReducer( state = initialState, action) {
     switch (action.type) {
         case ADD_TODO:
-            return [
+            return  {
                 ...state,
-                {
-                    id: action.todoId,
+                todoList: [ {
+                    todoId: action.todoId,
                     todoText: action.todoText
-                }
-            ];
+                    }
+                ]
+            }
         case REMOVE_TODO:
-            return state.filter(todo => todo.todoId !== action.todoId);
+
+            return { todoList: state.todoList.filter(todo =>
+                    todo.todoId !== action.todoId
+                )}
 
         case REMOVE_ALL_TODO:
             return initialState;
