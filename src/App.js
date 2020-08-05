4ed356fd49a79269579096288983a520d5e972aa
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-05 10:26:31 -0500

    Cleaned the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.scss';
-import { connect } from "react-redux";
 import TodoList from "./components/TodoList";
-import { AddTodo } from "./components/AddTodo";
+import  AddTodo  from "./components/AddTodo";
 import { Header } from "./components/Header";
-import {addTodoAction, removeAllTodoAction, removeTodoAction} from "./actions";
 
 export default class App extends React.Component {
 
 
     render() {
         return(
-
             <div className="d-flex flex-column">
                 <Header />
                 <AddTodo />
                 <TodoList />
             </div>
-
         );
     }
 }
