955ae34307857ab8ac00f339d2d4da17bd0d56f9
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-05 10:26:49 -0500

    Completed the addTodo event

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,12 +1,31 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodoAction } from "../actions.js";
 
-export class AddTodo extends Component {
+class AddTodo extends Component {
+
+    state = { content: ''};
+
+    handleChange = (event) => {
+        this.setState({
+            content: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.addTodoAction(this.state.content);
+        this.setState( {
+            content: ''
+        })
+    };
 
     render() {
         return (
             <div className="d-flex justify-content-center col-md-8 offset-md-2">
-                <form>
-                    <input className="todo-input mr-2" size="68" type="text"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="todo-input mr-2" size="68" type="text" value={this.state.content}
+                                    onChange={this.handleChange}/>
                     <button className="add-todo-btn">
                         <strong>Agregar</strong>
                     </button>
@@ -15,3 +34,9 @@ export class AddTodo extends Component {
         );
     }
 }
+
+const mapDispatchToProps = {
+    addTodoAction
+};
+
+export default connect(null, mapDispatchToProps)(AddTodo);
