cee3e0fd9a92fdc75c3d15a1802f1fd15cf1b9b1
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-05 14:07:57 -0500

    Cleaned the code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,11 +13,12 @@ class TodoList extends Component {
     render() {
         if (this.props.todoList.length) {
             return (
-                <div className="d-flex flex-column col-md-8 offset-md-2">
-                    <div className="mb-0 mt-3 py-3 px-3 mx-5 d-flex justify-content-between header-todo-list">
-                        <h4>
-                            toDo list by Iron Bit
-                        </h4>
+                <div className="d-flex flex-column">
+                    <div className="mb-0 mt-3 py-3 px-1 d-flex justify-content-between header-todo-list">
+                            <h4>
+                                toDo list by Iron Bit
+                            </h4>
+
                         <form onSubmit={this.removeAllTodo}>
                             <input type="image" alt="remove-all" src="icons/recycle-bin.svg"
                                    width="25" height="25"/>
@@ -32,8 +33,8 @@ class TodoList extends Component {
             )
         } else {
             return (
-                <div className="d-flex flex-column col-md-8 offset-md-2">
-                    <div className="mb-0 mt-3 py-3 px-3 mx-5 d-flex justify-content-between header-todo-list">
+                <div className="d-flex flex-column">
+                    <div className="mb-0 mt-3 py-3 d-flex justify-content-between header-todo-list">
                         <h4>
                             toDo list by Iron Bit
                         </h4>
@@ -43,7 +44,7 @@ class TodoList extends Component {
                         </form>
                     </div>
 
-                    <div className="d-flex py-3 px-3 mx-5 align-items-center
+                    <div className="d-flex py-3 align-items-center
                         justify-content-center item-todo">
                         <h4 className="text-center">No hay ToDo's, tomate un café!</h4>
                     </div>
